9882b29ec2bded22c6f872b7293a35f3d5740cfb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-07 16:55:51 -0300

    exercício_aula1_p1_final
    Finalização da primeira parte do exercício aula1.

--- aula1_/src/Exemplo1.java
+++ aula1_/src/Exemplo1.java
@@ -26,17 +26,25 @@ public class Exemplo1 {// programa para teste em aula
     public static void main(String[] args) {
         
         // TODO code application logic here
-        String ra,nome,materia;
-        double nt1,nt2,media;
+        String ra,nome,materia,status;// variaveis de uso geral
+        double nt1,nt2,media; // variaveis de uso geral
+        
         ra=JOptionPane.showInputDialog("Digite seu número de RA_");
         nome=JOptionPane.showInputDialog("Digite seu nome_");
         materia=JOptionPane.showInputDialog("Digite a disciplina_");
         nt1=Double.parseDouble(JOptionPane.showInputDialog("Digite a nota da sua AV1_"));
         nt2=Double.parseDouble(JOptionPane.showInputDialog("Digite a nota da sua AV2_"));
         
-        media=(nt1+nt2)/2;
+        media=(nt1+nt2)/2;// linha para calcular a media
         
-        JOptionPane.showMessageDialog(null,nome+",Sua média é"+media);
+        //  comando para verificar se o aluno foi reprovado.
+        if( media<6){
+            status="Reprovado";
+        } else{
+            status="Aprovado";
+        }
+        
+        JOptionPane.showMessageDialog(null,nome+ " ,Sua média é " +media+ ". Você foi " +status);
     }
     
 }
