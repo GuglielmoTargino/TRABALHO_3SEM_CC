aea25d844676975fbc2c8284f330fa475be5bbff
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-10 23:11:56 -0300

    aula5_classe_metodos
    Aula 5 online utilizando  classes e o métodos.

---
+++ aul20230310/src/Calcular.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+public class Calcular {
+    //  public static int somar_(int x1, int x2, int x3)
+    public int somar_(int x1, int x2, int x3){ //Se tirar o static, devo criar o objeto da classe <Calcular
+        return x1+x2+x3;
+    }
+    
+}
---
+++ aul20230310/src/TiposPrimitivosVariaveis.java
@@ -0,0 +1,47 @@
+
+import javax.swing.JOptionPane;
+
+/*
+    |** Programa´para utilizar classes e metodos **|
+ */
+public class TiposPrimitivosVariaveis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        double nr1,nr2,nr3, resu;
+        int x1, x2, x3;
+        
+        nr1=Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro numero _"));
+        nr2=Double.parseDouble(JOptionPane.showInputDialog("Digite o sugundo  numero _"));
+        nr3=Double.parseDouble(JOptionPane.showInputDialog("Digite o terceiro numero _"));
+        
+        x1=Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro x _"));
+        x2=Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo x _"));
+        x3=Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro x _"));
+        
+        
+        // resu=Calcular.somar_(x1, x2, x3);// comando para usar o static lá na clase
+        
+        Calcular calc;// calc é o objeto criado da classe <Calcular>
+        calc= new Calcular();
+        
+        resu=calc.somar_(x1, x2, x3);// comando para chamar a clasee <Calcular>
+        
+        
+        
+        JOptionPane.showMessageDialog(null,"A soma de x é "+ resu);
+        
+        
+        
+                
+        
+
+       
+
+    }
+
+}
